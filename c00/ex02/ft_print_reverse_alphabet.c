a4924422bbf707b866f47e536784fbb454bf33a0
bazaluga <bazaluga@student.42.fr>
2023-08-02 20:09:52 +0200

    start c00

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 18:43:47 by bazaluga          #+#    #+#             */
+/*   Updated: 2023/08/02 18:48:02 by bazaluga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
