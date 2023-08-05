b9f7667dcdb34decddc9aa2b3f8701631b749563
bazaluga <bazaluga@student.42.fr>
2023-08-05 23:00:12 +0200

    add address verification in swap

--- c01/ex02/ft_swap.c
+++ c01/ex02/ft_swap.c
@@ -6,15 +6,18 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 12:42:52 by bazaluga          #+#    #+#             */
-/*   Updated: 2023/08/05 21:18:58 by bazaluga         ###   ########.fr       */
+/*   Updated: 2023/08/05 22:54:07 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_swap(int *a, int *b)
 {
-	*a += *b;
-	*b = *a - *b;
-	*a -= *b;
+	if (a != b)
+	{
+		*a += *b;
+		*b = *a - *b;
+		*a -= *b;
+	}
 }
 
 /*#include <stdio.h>
