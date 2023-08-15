2c948b136076b914799ed0131d080f8a7dea3450
bazaluga <bazaluga@student.42.fr>
2023-08-15 19:49:12 +0200

    c04 -> ex04

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 18:34:21 by bazaluga          #+#    #+#             */
+/*   Updated: 2023/08/15 19:18:34 by bazaluga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	nb;
+	int	i;
+	int	sign;
+
+	i = 0;
+	while (str[i] && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
+		i++;
+	sign = 1;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	nb = 0;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + str[i] - '0';
+		i++;
+	}
+	return (nb * sign);
+}
+
+/*#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%d\n", ft_atoi(argv[1]));
+	return (0);
+}*/
---
+++ c04/ex04/ft_putnbr_base.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 19:19:17 by bazaluga          #+#    #+#             */
+/*   Updated: 2023/08/15 19:47:53 by bazaluga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	valid_base(char *base, int *len)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = i;
+		while (base[++j])
+			if (base[i] == base[j])
+				return (0);
+		i++;
+	}
+	*len = i;
+	if (i > 2)
+		return (1);
+	return (0);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	len_base;
+
+	if (!valid_base(base, &len_base) || len_base < 2)
+		return ;
+	if (nbr >= len_base)
+		ft_putnbr_base(nbr / len_base, base);
+	ft_putchar(base[nbr % len_base]);
+}
+
+/*int	main(void)
+{
+	ft_putnbr_base(42, "0123456789abcdef");
+}*/
