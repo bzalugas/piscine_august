497e456d99f39dd72888fb028009c0d5fd5de531
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2023-08-18 11:05:36 +0200

    continue c05

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -6,14 +6,9 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 21:09:53 by bazaluga          #+#    #+#             */
-/*   Updated: 2023/08/17 02:13:29 by bazaluga         ###   ########.fr       */
+/*   Updated: 2023/08/18 10:28:36 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	is_odd(int n)
-{
-	return (n % 2 != 0);
-}
 
 int	ft_sqrt(int nb)
 {
@@ -36,8 +31,7 @@ int	main(void)
 {
 	int	n;
 
-	n = 1073741824;
-	n = -2;
+	n = 2009370276;
 	printf("sqrt(%d) = %d\n", n, ft_sqrt(n));
 	return (0);
 }*/
--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,13 +6,54 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 14:24:30 by bazaluga          #+#    #+#             */
-/*   Updated: 2023/08/17 14:29:33 by bazaluga         ###   ########.fr       */
+/*   Updated: 2023/08/18 11:05:23 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_sqrt(int nb)
+{
+	int	n;
+	int	i;
+
+	n = 1;
+	i = 0;
+	while (nb > 0)
+	{
+		nb -= n;
+		n += 2;
+		i++;
+	}
+	return (i);
+}
 
 int	ft_is_prime(int nb)
 {
 	int	max;
+	int	i;
 
-
+	if (nb < 2)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	max = ft_sqrt(nb);
+	i = 2;
+	while (i < max)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
+
+/*#include <stdio.h>
+int	main(void)
+{
+	int	n;
+
+	n = 2147483647;
+	printf("%d is %s\n", n, ft_is_prime(n) ? "prime" : "not prime");
+	return (0);
+}*/
