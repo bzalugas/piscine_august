792c56f2ae164ed432bd23f1612e4bb76dd4c14a
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2023-08-18 10:19:30 +0200

    start c07

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 00:15:19 by bazaluga          #+#    #+#             */
+/*   Updated: 2023/08/18 10:17:54 by bazaluga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	unsigned int	len;
+	char			*dst;
+	unsigned int	i;
+
+	len = ft_strlen(src);
+	dst = malloc(sizeof(char) * (len + 1));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+/*/#include <stdio.h>
+int	main(void)
+{
+	char	*d;
+
+	d = ft_strdup("salut les amis");
+	printf("%s\n", d);
+	return (0);
+}*/
